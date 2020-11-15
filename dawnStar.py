f6dd0f184302811b8b89d7084912256772a93633
saha65536 <70512032+saha65536@users.noreply.github.com>
2020-11-15 22:54:54 +0800

    Add files via upload
    调整模型，更加精简

--- dawnStar.py
+++ dawnStar.py
@@ -10,7 +10,7 @@ import pandas as pd# 导入DataFrame数据
 
 class DawnStar:#黎明之星形态
     def __init__(self):   
-        self.insStockBase = StockBase('2019-01-01','2019-12-31','2020-09','2020-10')
+        self.insStockBase = StockBase('2019-01-01','2020-12-31','2020-09','2020-10')
         self.arr_codes = []
         self.arr_names = []
         self.arr_dates = []
@@ -45,47 +45,55 @@ class DawnStar:#黎明之星形态
         bFind = False
         bIndex = 1
         for p in range(1,len(df)-2):
-            #第一天收盘要跌
-            if df.iloc[p]['Open'] < df.iloc[p]['Close'] :
-                continue
             m = p + 1
             n = p + 2
-            #中间这天的最高价不能高于第一天的最高价
-            if df.iloc[m]['High'] > df.iloc[p]['High']:
+            #第一天收盘要跌
+            if df.iloc[p]['Volume'] == 0:
                 continue
+            if df.iloc[p]['Open'] < df.iloc[p]['Close'] :
+                continue
+            #第一天的阴线很长
+            #if df.iloc[p]['pctChg'] > -3:
+            #    continue
+            #第一天的实体柱要长
+            if (df.iloc[p]['Open']-df.iloc[p]['Close'])/(df.iloc[p]['Close']-df.iloc[p]['Low']) < 2:
+                continue
+            
+            #中间这天要跳空低开
+            if df.iloc[m]['High'] > df.iloc[p]['Low']:
+                continue                       
             #中间这天的下影线是实体柱的两倍以上
             if df.iloc[m]['Open'] != df.iloc[m]['Close'] and (min(df.iloc[m]['Open'],df.iloc[m]['Close']) - df.iloc[m]['Low']) / abs(df.iloc[m]['Open'] - df.iloc[m]['Close']) < 2:
                 continue
             #中间这天最低价要比收盘价跌超过3%
-            if (min(df.iloc[m]['Open'],df.iloc[m]['Close']) - df.iloc[m]['Low'])/df.iloc[m]['Close']<0.03:
-                continue
+            #if (min(df.iloc[m]['Open'],df.iloc[m]['Close']) - df.iloc[m]['Low'])/df.iloc[m]['Close']<0.03:
+            #    continue
             #最后这天必须是阳线
             if df.iloc[n]['Open'] > df.iloc[n]['Close']:
                 continue
             #最后这天收盘价必须比第一天的最高点高
             if df.iloc[n]['Close'] < df.iloc[p]['High']:
                 continue
-            #最后这天的量必须大于前两天
-            if df.iloc[n]['Volume'] < df.iloc[p]['Volume'] or df.iloc[n]['Volume'] < df.iloc[m]['Volume']:
-                continue    
-            if df.iloc[n]['Volume'] < df.iloc[p]['Volume']*1.5:
-                continue  
-            #如果最后这天的量均大于第一天和第二天的3倍以上，则最后一天不能有上影线
-            if df.iloc[n]['Volume'] > 3*df.iloc[p]['Volume'] and df.iloc[n]['Volume'] > 3*df.iloc[m]['Volume'] and df.iloc[n]['High'] != df.iloc[n]['Close']:
+            if (df.iloc[n]['High'] - df.iloc[n]['Close']) / df.iloc[n]['Close'] > 0.02:
                 continue
+            #最后这天的量必须大于前两天
+            #if df.iloc[n]['Volume'] < df.iloc[p]['Volume'] or df.iloc[n]['Volume'] < df.iloc[m]['Volume']:
+            #    continue    
+            
             self.arr_codes.append(stock_code)   
             self.arr_names.append(stock_name)
             self.arr_dates.append(df.iloc[p]['od'])
             bFind = True
             bIndex = p + 3
+            print("find one bingo total: " + str(len(self.arr_codes)))
             break
         
         if bFind:
             profit = 0
             buyPrice =  df.iloc[bIndex]['Open']
             for i in range(bIndex + 1,bIndex+10):
-                if df.iloc[i]['High'] / buyPrice > 1.03:
-                    profit = 1.03
+                if df.iloc[i]['High'] / buyPrice > 1.06:
+                    profit = 1.06
                     break
             if 0 == profit:
                 profit = df.iloc[i]['Close'] / buyPrice    
